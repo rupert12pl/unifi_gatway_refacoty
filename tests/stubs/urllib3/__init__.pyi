7a9173b189b8463e29851ed805e2a4f67e686957
rupert12pl <79536582+rupert12pl@users.noreply.github.com>
2025-10-03 20:01:27 +0200

    Remove unused InsecureRequestWarning stub import

--- tests/stubs/urllib3/__init__.pyi
+++ tests/stubs/urllib3/__init__.pyi
@@ -1,6 +1,4 @@
 from builtins import Warning
 from typing import Optional, Type
 
-from .exceptions import InsecureRequestWarning
-
 def disable_warnings(category: Optional[Type[Warning]] = ...) -> None: ...
